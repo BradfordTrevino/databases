118baf08bb72b22447591b6a06acfa3d6d96cc9d
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-03-16 17:30:30 -0700

    Completed designing multi-table schema!

--- server/schema.sql
+++ server/schema.sql
@@ -2,10 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE users (
   /* Describe your table here.*/
+  username_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20)
 );
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  rooms_id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(20)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(140),
+  username_id INT,
+  rooms_id INT,
+  FOREIGN KEY(username_id)
+    REFERENCES users(username_id),
+  FOREIGN KEY(rooms_id)
+    REFERENCES rooms(rooms_id)
+);
 /* Create other tables and define schemas for them here! */
 
 
